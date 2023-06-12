ee6e7cfd863bf68806d413b74483f898f4a745bd
KishoreMenda <m.kishore0309@gmail.com>
2023-06-12 14:53:33 -0700

    add service and routes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero = 'Windstorm';
+  heroes : Hero[] = [];
+  constructor(private heroService : HeroService, private messageService: MessageService){
+
+  }
+  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  selectedHero?:Hero;
+  onSelect(hero: Hero):void{
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 }
